638ff8bf30d99b8bdb670e9c1a27f26be1e2d301
Anurag Roy <38952781+Anuragroyan@users.noreply.github.com>
2024-05-03 01:04:51 +0530

    this app is advance drawing app using latest android version 14 and basically it can be used by kids for playing or draw image, this app includes list of feature 1- draw an image on plane canvas 2- draw an image on picture from the phone 3- save the image 4- undo or redo operation 5- share the image online.

--- app/src/main/java/com/example/drawingapp/DrawingView.kt
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -11,12 +11,13 @@ import android.view.View
 
 
 class DrawingView(context: Context, attrs : AttributeSet): View(context, attrs){
-    private var mDrawPath : CustomPath? = null
-    private var mCanvasBitmap : Bitmap? = null
-    private var mDrawPaint : Paint? = null
-    private var mCanvasPaint : Paint? = null
-    private var mBrushSize : Float = 0.toFloat()
-    private var color = Color.BLACK
+    private var mDrawPath : CustomPath? = null  // custom path inner class to use
+    private var mCanvasBitmap : Bitmap? = null  // an instance of bitmap
+    private var mDrawPaint : Paint? = null  // paint class holds the style and color info about
+    // how to draw geometrics ,text and bitmap.
+    private var mCanvasPaint : Paint? = null // an instance of canvas paint view
+    private var mBrushSize : Float = 0.toFloat() // variable for stroke/brush size to draw on the canvas
+    private var color = Color.BLACK // variable to hold a color of the strokes
     private var canvas : Canvas? = null
     private val mPaths = ArrayList<CustomPath>()
     private val mUndoPaths = ArrayList<CustomPath>()
@@ -24,6 +25,7 @@ class DrawingView(context: Context, attrs : AttributeSet): View(context, attrs){
         setupDrawing()
     }
 
+    // func of undo
     fun onClickUndo(){
         if(mPaths.size>0){
             mUndoPaths.add(mPaths.removeAt(mPaths.size-1))
@@ -31,6 +33,7 @@ class DrawingView(context: Context, attrs : AttributeSet): View(context, attrs){
         }
     }
 
+    // func of redo
     fun onClickRedo(){
         if(mUndoPaths.size>0){
             mPaths.add(mUndoPaths.removeAt(mUndoPaths.size-1))
@@ -38,22 +41,28 @@ class DrawingView(context: Context, attrs : AttributeSet): View(context, attrs){
         }
     }
 
+    // func of drawing on canvas
     private fun setupDrawing(){
         mDrawPaint = Paint()
         mDrawPath = CustomPath(color,mBrushSize)
         mDrawPaint!!.color = color
-        mDrawPaint!!.style = Paint.Style.STROKE
-        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
-        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
-        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mDrawPaint!!.style = Paint.Style.STROKE  // this is to draw a stroke style
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND // this is for store join
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND // this is for stroke cap
+        mCanvasPaint = Paint(Paint.DITHER_FLAG) // paint flag that enables dithering when blitting
     }
 
+    // func to change stroke size
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
         mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
         canvas = Canvas(mCanvasBitmap!!)
     }
 
+    // Draw the specified bitmap, with its top/left corner at (x,y), using the specified paint,
+    // transformed by the current matrix.
+    // If the bitmap and canvas have different densities, this function will take care of,
+    // automatically scaling the bitmap to draw at the same density as the canvas.
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
@@ -69,18 +78,19 @@ class DrawingView(context: Context, attrs : AttributeSet): View(context, attrs){
         }
     }
 
-
+   // func acts as an event listener when a touch event is detected on the device
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        val touchX = event?.x
-        val touchY = event?.y
+        val touchX = event?.x  // touch event of x coordinate
+        val touchY = event?.y  // touch event of y coordinate
         when(event?.action){
             MotionEvent.ACTION_DOWN -> {
                 mDrawPath!!.color = color
                 mDrawPath!!.brushThickness = mBrushSize
-                mDrawPath!!.reset()
+                mDrawPath!!.reset() // clear any lines and curves from the path, making it empty
                 if(touchX!=null){
                     if(touchY!=null){
+                        // set the beginning of the next corner to the point (x,y)
                         mDrawPath!!.moveTo(touchX, touchY)
                     }
                 }
@@ -88,6 +98,7 @@ class DrawingView(context: Context, attrs : AttributeSet): View(context, attrs){
             MotionEvent.ACTION_MOVE -> {
                 if(touchX!=null){
                     if(touchY!=null){
+                        // add a line from the last point to the specified point (x,y)
                         mDrawPath!!.lineTo(touchX,touchY)
                     }
                 }
@@ -102,6 +113,7 @@ class DrawingView(context: Context, attrs : AttributeSet): View(context, attrs){
         return true
     }
 
+    // func for either the brush or the eraser
     fun setSizeForBrush(newSize: Float){
         mBrushSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
             newSize, resources.displayMetrics
@@ -109,6 +121,7 @@ class DrawingView(context: Context, attrs : AttributeSet): View(context, attrs){
         mDrawPaint!!.strokeWidth = mBrushSize
     }
 
+    // func for changing user desires color
     fun setColor(newColor: String){
         color = Color.parseColor(newColor.toString())
         mDrawPaint!!.color = color
--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -35,6 +35,7 @@ import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     private var drawingView : DrawingView? = null
+ // variable for current color is picked from color pallet
     private var mImageButtonCurrentPaint : ImageButton? = null
     var customProgressDialog : Dialog? = null
     val openGalleryLauncher : ActivityResultLauncher<Intent> =
@@ -45,22 +46,27 @@ class MainActivity : AppCompatActivity() {
                 imageBackGround.setImageURI(result.data?.data)
             }
         }
+ // create requestPermission for multiplepermissions since we are requesting both read and write
     val requestPermission : ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
             permissions ->
             permissions.entries.forEach{
                 val permissionName = it.key
                 val isGranted = it.value
+             // if permission is granted show a popup to perform operation
                 if(isGranted){
                     Toast.makeText(
                         this@MainActivity,
                         "Permission granted now you can read the storage files.",
                         Toast.LENGTH_LONG
                     ).show()
+             // this code is for selecting picture from device
                     val pickIntent = Intent(Intent.ACTION_PICK,
                         MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
                     openGalleryLauncher.launch(pickIntent)
                 }else{
+             // displaying another popup if permission is not granted and
+                    // this time focus on read external storage
                     if(permissionName==Manifest.permission.READ_EXTERNAL_STORAGE){
                         Toast.makeText(
                             this@MainActivity,
@@ -98,18 +104,24 @@ class MainActivity : AppCompatActivity() {
         ibRedo.setOnClickListener{
             drawingView?.onClickRedo()
         }
+        // for save button from the layout
         val ibsave : ImageButton = findViewById(R.id.ib_save)
         ibsave.setOnClickListener{
+        // chech if permission is allowed
            if(isReadStorageAllowed()){
                showProgressDialog()
+               // launch a coroutine block
                lifecycleScope.launch {
+                   // reference the frame layout
                     val flDrawingView: FrameLayout = findViewById(R.id.fl_drawing_view_container)
+                   // save the image to the device
                    saveBitmapFile(getBitmapFromView(flDrawingView))
                }
            }
         }
     }
 
+    // func is uses to launch the dialog to select different brush sizes
     private fun showBrushSizeChooserDialog(){
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
@@ -131,12 +143,17 @@ class MainActivity : AppCompatActivity() {
         }
        brushDialog.show()
     }
-
+    // func is called when color is clicked from the pallet_normal
     fun paintClicked(view: View){
        if(view !== mImageButtonCurrentPaint){
+           // update the color
            val imageButton = view as ImageButton
+           // tag is used for swaping the current color with previous color
+           // tag stores the selected view
            val colorTage = imageButton.tag.toString()
+           // color is set as per the selected tag here
            drawingView?.setColor(colorTage)
+           // swap the background for last active and currently active image button
            imageButton.setImageDrawable(
                ContextCompat.getDrawable(
                    this,
@@ -149,24 +166,34 @@ class MainActivity : AppCompatActivity() {
                    R.drawable.pallet_normal
                )
            )
+           // current view is updated with selected view in the form of imagebutton
            mImageButtonCurrentPaint = view
        }
     }
-
+    // func to check the permission status
     private fun isReadStorageAllowed(): Boolean {
+        // getting the permission status and
+        // checkselfPermission is particular permission is granted
         val result = ContextCompat.checkSelfPermission(this,
               Manifest.permission.READ_EXTERNAL_STORAGE
             )
+        // if permission is granted returning true and if permission is not granted returning false
         return result == PackageManager.PERMISSION_GRANTED
     }
 
+    // func to requeststorage permission
     private fun requestStoragePermission(){
+        // chech if the permission was denied and show rationale
        if(ActivityCompat.shouldShowRequestPermissionRationale(
            this,
            Manifest.permission.READ_EXTERNAL_STORAGE)){
+           // call the rationale dialog to tell the user why they need to allow permission request
            showRationaleDialog(" Drawing_App_Kids "," Drawing_App_Kids"
            + " needs to Access your External Storage ")
        }else{
+           // you can directly ask for the permission
+           // if it has not been denied then request for permission
+           // the registered ActivityResultCallback get the result of this request
            requestPermission.launch(arrayOf(
                Manifest.permission.READ_EXTERNAL_STORAGE,
                Manifest.permission.WRITE_EXTERNAL_STORAGE
@@ -174,6 +201,8 @@ class MainActivity : AppCompatActivity() {
        }
     }
 
+    // create rationale dialog shows rationale dialog for display why the app needs permission
+    // only shown if the user has denied the permission request previously
     private fun showRationaleDialog(
         title : String,
         message : String
@@ -187,32 +216,49 @@ class MainActivity : AppCompatActivity() {
         builder.create().show()
     }
     private fun getBitmapFromView(view: View): Bitmap {
+        // define a bitmap with the same size as the view
+        // createBitmap: returns a mutable bitmap with the specified width and height
         val returnedBitmap = Bitmap.createBitmap(
             view.width, view.height, Bitmap.Config.ARGB_8888)
+        // bind a cnavas to it
         val canvas = Canvas(returnedBitmap)
         val bgDrawable = view.background
         if(bgDrawable!= null){
+            // has background drawable, then draw it on the canvas
             bgDrawable.draw(canvas)
         }else{
+            // does not have background drawbale, then draw white background on the canvas
             canvas.drawColor(Color.WHITE)
         }
+        // draw the view on the canvas
         view.draw(canvas)
+        // return the bitmap
         return returnedBitmap
     }
+    // save the image on the device
     private suspend fun saveBitmapFile(mBitmap: Bitmap?): String {
         var result = ""
         withContext(Dispatchers.IO){
             if(mBitmap!=null){
                 try{
+                    // creates a new byte array output stream
                     val bytes = ByteArrayOutputStream()
+                    // Write a compressed version of the bitmap to the specified outputstream.
+                    // If this returns true, the bitmap can be reconstructed by passing a
+                    // corresponding inputstream to BitmapFactory.decodeStream()
                     mBitmap.compress(Bitmap.CompressFormat.PNG, 90, bytes)
                     val f = File(externalCacheDir?.absoluteFile.toString() +
                         File.separator + "DrawingAppForKids_" +
                         System.currentTimeMillis() / 1000 + ".png"
                     )
+                    // Creates a file output stream to write to the file represented by the specified object
                     val fo = FileOutputStream(f)
+                    // Writes bytes from the specified byte array to this file output stream.
                     fo.write(bytes.toByteArray())
+                    // Closes this file output stream and releases any system resources associated with this stream.
+                    // This file output stream may no longer be used for writing bytes
                     fo.close()
+                    // The file absolute path is return as a result
                     result = f.absolutePath
                     runOnUiThread {
                         cancelProgressDialog()
@@ -240,12 +286,17 @@ class MainActivity : AppCompatActivity() {
         return result
     }
 
+    // func is used to show the custom progress dialog
     private fun showProgressDialog(){
         customProgressDialog = Dialog(this@MainActivity)
+        // Set the screen content from a layout resource
+        // The resource will be inflated, adding all top-level views to the screen
         customProgressDialog?.setContentView(R.layout.dialog_custom_progress)
+        // Start the dialog and display it on screen
         customProgressDialog?.show()
     }
 
+    // func is used to dismiss the progress dialog if it is visible to user
     private fun cancelProgressDialog(){
         if(customProgressDialog!=null){
             customProgressDialog?.dismiss()
